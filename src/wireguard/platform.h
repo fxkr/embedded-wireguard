7639ef42c6e88d01377875d2eda00d3149e28420
Felix Kaiser <felix.kaiser@fxkr.net>
2021-03-14 18:12:20 -0700

    Add endianness conversion macros

--- src/wireguard/platform.h
+++ src/wireguard/platform.h
@@ -6,6 +6,30 @@
 #include <stdint.h>
 #include <string.h>
 
+// Byte order handling functions
+#ifndef __BYTE_ORDER__
+#error __BYTE_ORDER__ is undefined
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define wg_htole32(x) (x)
+#define wg_le32toh(x) (x)
+#define wg_htole64(x) (x)
+#define wg_le64toh(x) (x)
+#define wg_htobe32(x) __builtin_bswap32(x)
+#define wg_be32toh(x) __builtin_bswap32(x)
+#define wg_htobe64(x) __builtin_bswap64(x)
+#define wg_be64toh(x) __builtin_bswap64(x)
+#elif __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#define wg_htole32(x) __builtin_bswap32(x)
+#define wg_le32toh(x) __builtin_bswap32(x)
+#define wg_htole64(x) __builtin_bswap64(x)
+#define wg_le64toh(x) __builtin_bswap64(x)
+#define wg_htobe32(x) (x)
+#define wg_be32toh(x) (x)
+#define wg_htobe64(x) (x)
+#define wg_be64toh(x) (x)
+#else
+#error Bad __BYTE_ORDER__
+#endif
 // TAI64N format as specified by WireGuard spec.
 struct wg_timestamp_fields {
 	// Seconds since 1970 TAI. Big endian.
