4737b645f382f21c7a38b59efa04f966c468ec7e
Felix Kaiser <felix.kaiser@fxkr.net>
2021-03-14 18:25:02 -0700

    Add WireGuard peer and session state structs

--- src/wireguard/wireguard.h
+++ src/wireguard/wireguard.h
@@ -78,6 +78,73 @@ struct wg_window {
 	uint64_t last_seq;
 };
 
+// Dynamic state that is calculated during and after a handshake,
+// such as ephemeral keys, transport keys and counters.
+// Any handshake will start with all of these fields zeroed.
+//
+// We deviate from the whitepaper in that we split variables by local/remote,
+// not initiator/responder. This simplifies the code because each peer can act
+// either as initiator or as responder. But it means variable names don't quite
+// map 1:1 to the whitepaper.
+struct wg_session {
+	uint32_t local_index;  // Name in whitepaper: I_*
+	uint32_t remote_index; // Name in whitepaper: I_*
+
+	union wg_key local_ephemeral_private; // Name in whitepaper: E^priv_*
+	union wg_key local_ephemeral_public;  // Name in whitepaper: E^pub_*
+	union wg_key remote_ephemeral_public; // Name in whitepaper: E^pub_*
+
+	uint64_t sending_key_counter;	    // Name in whitepaper: N^send_*
+	union wg_symmetric_key sending_key; // Name in whitepaper: T^send_*
+	bool sending_key_valid;		    // False until sending key has been derived.
+
+	uint64_t receiving_key_counter;	      // Name in whitepaper: N^recv_*
+	union wg_symmetric_key receiving_key; // Name in whitepaper: T^recv_*
+	bool receiving_key_valid;	      // False until receiving key has been derived.
+
+	union wg_hash local_hash;  // Name in whitepaper: H_*
+	union wg_hash remote_hash; // Name in whitepaper: H_*
+	union wg_key chaining_key; // Name in whitepaper: C_*
+
+	// The MAC1 of the last handshake initiation or reply we sent.
+	/// Used to verify incoming cookie message.
+	union wg_mac last_sent_mac1;
+	bool received_cookie_valid; // False until last_sent_mac1 has been set.
+
+	// Last correctly received cookie, used to calculate MAC2 in sent handshake messages.
+	union wg_cookie received_cookie;
+	union wg_timestamp received_cookie_timestamp; // When received_cookie was received.
+
+	// Used to track received sequence numbers to prevent replay attacks.
+	struct wg_window window;
+};
+
+// Represents a tunnel to a specific peer. Contains both static configuration
+// and more dynamic session state.
+struct wg_peer {
+	struct wg_session session;
+
+	union wg_symmetric_key preshared_key; // Name in whitepaper: Q. Optional - may be all zeroes.
+	union wg_key local_static_public;     // Name in whitepaper: S^pub_*
+	union wg_key local_static_private;    // Name in whitepaper: S^priv_*
+	union wg_key remote_static_public;    // Name in whitepaper: S^pub_*
+
+	// Maximum MTU we can use to send packets to the peer.
+	// This is the underlay MTU. The useable overlay MTU will be less than this by:
+	//
+	// * Underlay IP header (20 bytes IPv4, or 40 bytes IPv6, plus options respectively)
+	// * Underlay UDP header (8 byte)
+	// * WireGuard transport header (32 byte)
+	size_t mtu;
+
+	// True if the local peer currently requires valid MAC2 on received handshake messages.
+	bool cookie_required;
+
+	// Random data used to generate cookies. Supposed to change every 2 minutes.
+	// Name in whitepaper: R_m
+	union wg_key cookie_secret;
+};
+
 extern const uint8_t wg_construction[37]; // No zero termination!
 extern const uint8_t wg_identifier[34];	  // No zero termination!
 extern const uint8_t wg_label_mac1[8];	  // No zero termination!
