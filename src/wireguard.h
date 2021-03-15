2925d3f262038f2e8895da4268dbc13e93c7e2e1
Felix Kaiser <felix.kaiser@fxkr.net>
2021-03-14 18:27:33 -0700

    Add public WireGuard API header

---
+++ src/wireguard.h
@@ -0,0 +1,19 @@
+#ifndef WG_WIREGUARD_API_H
+#define WG_WIREGUARD_API_H
+
+// This is the public interface to Embedded WireGuard.
+// Include only this header: #include <wireguard.h>
+//
+// Unless documented differently, any function with an int return type
+// returns 0 for success, and a non-zero value for failure.
+// That value is currently always 1, however callers must not depend on this
+// and must treat any non-zero value as a failure.
+//
+// Unless documented differently, functions have all-or-nothing behavior.
+// In other words, any function that returns an error has no side effects.
+
+// This function must be called at least once before any other Embedded WireGuard
+// functions are used. It is safe to call it any number of times.
+int __attribute__((warn_unused_result)) wg_init(void);
+
+#endif
