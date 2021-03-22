0b180a1b91934bbc254ae626868c030c79756aac
Felix Kaiser <felix.kaiser@fxkr.net>
2021-03-22 00:57:03 -0700

    Update coverage script with new environment name

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x -euo pipefail
-pio test -e native-test
+pio test -e native
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-lcov -d .pio/build/native-test -c -o lcov.info --exclude "$(pwd)/test/tmp_pio_test_transport.cpp"
+lcov -d .pio/build/native -c -o lcov.info --exclude "$(pwd)/test/tmp_pio_test_transport.cpp"
 genhtml -o cov/ --demangle-cpp lcov.info
